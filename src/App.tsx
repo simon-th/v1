67c9055cccae7ae5f407c52be9d16bc4fc0b61ad
Simon Hoque <simon.thoque@gmail.com>
2021-11-26 13:46:12 -0600

    Make change to verify CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Version 2021-11-26 13:14</p>
+        <p>Version 2021-11-26 13:45</p>
+        <p>Deployed using CI/CD</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
