82a064a2cf8cf34c196edd054877c355a7fcd952
Simon Hoque <simon.thoque@gmail.com>
2021-12-20 03:51:27 +0000

    Fix style

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Landing from "./landing/Landing";
 import reportWebVitals from "./reportWebVitals";
 
--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "../common/common.css";
-import "./landing.css"
+import "./landing.css";
 
 const Landing = function render() {
   return (
@@ -11,10 +11,30 @@ const Landing = function render() {
         <h1 className="Welcome-text">I&apos;m Simon Hoque.</h1>
         <h2 className="Welcome-subtext">Software Engineer @ Amazon</h2>
         <Container className="Landing-links-container">
-          <a className="Link-swe Landing-links" href="https://www.github.com/simon-th">Software Engineering</a>
-          <a className="Link-photo Landing-links" href="https://www.github.com/simon-th">Photography</a>
-          <a className="Link-travel Landing-links" href="https://www.github.com/simon-th">Travel</a>
-          <a className="Link-music Landing-links" href="https://www.github.com/simon-th">Music</a>
+          <a
+            className="Link-swe Landing-links"
+            href="https://www.github.com/simon-th"
+          >
+            Software Engineering
+          </a>
+          <a
+            className="Link-photo Landing-links"
+            href="https://www.github.com/simon-th"
+          >
+            Photography
+          </a>
+          <a
+            className="Link-travel Landing-links"
+            href="https://www.github.com/simon-th"
+          >
+            Travel
+          </a>
+          <a
+            className="Link-music Landing-links"
+            href="https://www.github.com/simon-th"
+          >
+            Music
+          </a>
         </Container>
       </Container>
     </div>
