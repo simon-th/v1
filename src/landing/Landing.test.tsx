24bde6443f6c377a3aa62d61442535f5e9f44899
Simon Hoque <simon.thoque@gmail.com>
2021-12-26 05:05:12 +0000

    Fix test

--- src/landing/Landing.test.tsx
+++ src/landing/Landing.test.tsx
@@ -3,5 +3,6 @@ import { render } from "@testing-library/react";
 import Landing from "./Landing";
 
 test("dummy test runs successfully", () => {
+  render(<Landing />);
   expect(true).toBeTruthy();
 });
--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import "../common/common.css";
 import "./landing.css";
 
@@ -12,44 +12,46 @@ import spotify from "../resources/icons/spotify.svg";
 const Landing = function render() {
   return (
     <div className="Base">
-      <Container className="Main-container flex-grow-1">
-        <p className="Welcome-text h1">Hi,</p>
-        <h1 className="Welcome-text">I&apos;m Simon Hoque.</h1>
-        <h2 className="Welcome-subtext">Software Engineer @ Amazon</h2>
-        <Container className="Landing-links-container">
-          <Link to="/dev" className="Color-accent-orange Landing-links">
-            Software Engineering
-          </Link>
-          <Link to="/photo" className="Color-accent-teal Landing-links">
-            Photography
-          </Link>
-          <Link to="/travel" className="Color-secondary-text Landing-links">
-            Travel
-          </Link>
+      <BrowserRouter>
+        <Container className="Main-container flex-grow-1">
+          <p className="Welcome-text h1">Hi,</p>
+          <h1 className="Welcome-text">I&apos;m Simon Hoque.</h1>
+          <h2 className="Welcome-subtext">Software Engineer @ Amazon</h2>
+          <Container className="Landing-links-container">
+            <Link to="/dev" className="Color-accent-orange Landing-links">
+              Software Engineering
+            </Link>
+            <Link to="/photo" className="Color-accent-teal Landing-links">
+              Photography
+            </Link>
+            <Link to="/travel" className="Color-secondary-text Landing-links">
+              Travel
+            </Link>
+          </Container>
+          <Row
+            className="gap-4 Social-icon-container"
+            xs="auto"
+            sm="auto"
+            md="auto"
+            lg="auto"
+            xl="auto"
+            xxl="auto"
+          >
+            <a href="https://www.github.com/simon-th">
+              <img src={github} className="Social-icon" alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/simon-hoque">
+              <img src={linkedin} className="Social-icon" alt="linkedin" />
+            </a>
+            <a href="https://www.instagram.com/_simonhoque">
+              <img src={instagram} className="Social-icon" alt="instagram" />
+            </a>
+            <a href="https://spoti.fi/3iTN4YT">
+              <img src={spotify} className="Social-icon" alt="spotify" />
+            </a>
+          </Row>
         </Container>
-        <Row
-          className="gap-4 Social-icon-container"
-          xs="auto"
-          sm="auto"
-          md="auto"
-          lg="auto"
-          xl="auto"
-          xxl="auto"
-        >
-          <a href="https://www.github.com/simon-th">
-            <img src={github} className="Social-icon" alt="github" />
-          </a>
-          <a href="https://www.linkedin.com/in/simon-hoque">
-            <img src={linkedin} className="Social-icon" alt="linkedin" />
-          </a>
-          <a href="https://www.instagram.com/_simonhoque">
-            <img src={instagram} className="Social-icon" alt="instagram" />
-          </a>
-          <a href="https://spoti.fi/3iTN4YT">
-            <img src={spotify} className="Social-icon" alt="spotify" />
-          </a>
-        </Row>
-      </Container>
+      </BrowserRouter>
     </div>
   );
 };
