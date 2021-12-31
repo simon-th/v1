61696a44694beb13a95ae7b5c3b2d0f2ffd2156a
Simon Hoque <simon.thoque@gmail.com>
2021-12-31 18:43:32 +0000

    Add experience position dropdown and modify styles

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Button } from "react-bootstrap";
 import "../styles/common.css";
 import ExperienceInfo from "../resources/info/experience.json";
 
@@ -7,24 +8,36 @@ interface PositionProps {
   position: string;
   time: string;
   details: string[];
+  initialDropdownStatus: boolean;
 }
 
 function getPositionDetails(positionDetails: string[]): JSX.Element[] {
   const details: JSX.Element[] = [];
   positionDetails.forEach((detail) => {
-    details.push(<li className="Site-small-p">{detail}</li>);
+    details.push(
+      <li style={{ paddingBottom: "4px" }} className="Site-small-p">
+        {detail}
+      </li>
+    );
   });
   return details;
 }
 
 const Position = function render(props: PositionProps) {
-  const { company, position, time, details } = props;
+  const { company, position, time, details, initialDropdownStatus } = props;
+  const [showDetails, setShowDetails] = React.useState(initialDropdownStatus);
   return (
-    <div style={{ paddingBottom: "1.5vh" }}>
+    <div>
       <div className="d-flex justify-content-between">
-        <h3 className="Site-h3">
-          {position} @ {company}
-        </h3>
+        <Button
+          variant="link"
+          className="Position-dropdown-button Site-h3 p-0"
+          onClick={() => setShowDetails(!showDetails)}
+        >
+          {`${
+            showDetails ? `\u25be` : `\u25b8`
+          }\u00a0\u00a0${position} @ ${company}`}
+        </Button>
         <h4
           className="Site-small-text Color-tertiary-text"
           style={{ paddingLeft: "2vw", whiteSpace: "nowrap" }}
@@ -32,7 +45,9 @@ const Position = function render(props: PositionProps) {
           {time}
         </h4>
       </div>
-      {getPositionDetails(details)}
+      {showDetails ? (
+        <ul style={{ paddingTop: "6px" }}>{getPositionDetails(details)}</ul>
+      ) : null}
     </div>
   );
 };
@@ -46,6 +61,7 @@ function getPreviousPositions(): JSX.Element[] {
         position={position.position}
         time={position.time}
         details={position.details}
+        initialDropdownStatus={false}
       />
     );
   });
@@ -57,14 +73,17 @@ const Experience = function render() {
     <div className="gap-3">
       <h1 className="Site-h1">Experience</h1>
       <h2 className="Site-h2 Color-accent-teal">Current Position</h2>
-      <Position
-        company={ExperienceInfo.currentPosition.company}
-        position={ExperienceInfo.currentPosition.position}
-        time={ExperienceInfo.currentPosition.time}
-        details={ExperienceInfo.currentPosition.details}
-      />
+      <div style={{ paddingBottom: "0.8vh" }}>
+        <Position
+          company={ExperienceInfo.currentPosition.company}
+          position={ExperienceInfo.currentPosition.position}
+          time={ExperienceInfo.currentPosition.time}
+          details={ExperienceInfo.currentPosition.details}
+          initialDropdownStatus
+        />
+      </div>
       <h2 className="Site-h2 Color-secondary-text">Previous Positions</h2>
-      {getPreviousPositions()}
+      <Container className="gap-2 p-0">{getPreviousPositions()}</Container>
     </div>
   );
 };
